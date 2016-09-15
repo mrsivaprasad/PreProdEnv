6a0434902fc2b0867a0be8a57e3f575f8221301f
M R Siva Prasad <mrsivaprasad@users.noreply.github.com>
2016-09-15 17:01:58 +0530

    Update Percentage.py

--- Percentage.py
+++ Percentage.py
@@ -4,7 +4,7 @@ def functionName( percentage ):
     if percentage < 40.0:
         raise Exception("BUILD FAILED :" +str(percentage))
     else:
-        echo "Successfully TESTED"
+        print "Successfully TESTED"
 text_file=open("TEST-PreProdEnvTestCases.txt","r")
 text_file.readline();
 data= text_file.readline();
