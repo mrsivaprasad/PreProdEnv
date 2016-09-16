0227786564b8b15f5109ee5e1c52dc6fef9c5902
M R Siva Prasad <mrsivaprasad@users.noreply.github.com>
2016-09-16 11:26:11 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,11 @@
+import yaml
+#######################################################
+#This python script involving accessing testEnv content present in config.yaml file
+#with open('config.yaml', 'r') as f:
+yaml_file= open('config.yaml','r')
+yaml_data= yaml.load(yaml_file)
+global s;
+global test_percent;
+s='preProdEnv'
+test_percent=yaml_data['test_Servers'][s]
+#######################################################
